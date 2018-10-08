144cd5ad9fbcaf040056bbed28e0b9b820251b2d
luoxu <>
2018-10-08 13:12:39 +0800

    init

---
+++ src/VultrInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoxulx
+ * Date: 2018/10/8
+ * Time: 上午12:19
+ */
+
+
+/**
+ * Vultr Client interface.
+ */
+interface VultrInterface
+{
+
+    const into = 'test';
+}
